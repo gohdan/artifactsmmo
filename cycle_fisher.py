03076eab90081af0bcd2d9f2f06fe5f8daf4a8b5
gohdan <gohdan@gohdan.ru>
2024-08-06 00:38:06 +0300

    Add shrimp and fight chicken

--- cycle_fisher.py
+++ cycle_fisher.py
@@ -3,8 +3,9 @@ character_type ="fisher"
 with open("functions.py") as functions:
     exec(functions.read())
 
-inventory_limit = 100
+inventory_limit = 50
 gudgeon_qty = 30
+shrimp_qty = 30
 
 while True:
 
@@ -16,6 +17,22 @@ while True:
     do_move(x, y)
     cycle_gathering(inventory_limit)
 
+    # shrimp (fishing 10)
+    print ("=== gather shrimp ===")
+    x, y = 5, 2
+    do_move(x, y)
+    cycle_gathering(inventory_limit)
+
+    # ======= FIGHTING ======
+
+    # chicken (1)
+    print ("=== fight chicken ===")
+    x, y = 0, 1
+    do_move(x, y)
+    #do_unequip("weapon")
+    #do_equip("wooden_staff", "weapon")
+    cycle_fight(10)
+
     # ======= BANKING ======
 
     # bank
@@ -24,4 +41,10 @@ while True:
     do_move(x, y)
 
     do_bank_deposit("gudgeon", gudgeon_qty)
+    do_bank_deposit("shrimp", shrimp_qty)
+    do_bank_deposit("golden_shrimp", 1)
+
+    do_bank_deposit("feather", 5)
+    do_bank_deposit("egg", 5)
+    do_bank_deposit("raw_chicken", 5)
 
