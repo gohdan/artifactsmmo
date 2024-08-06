9b03bb9a7a57b982e9a1aaf55b4afbe4c088b861
gohdan <gohdan@gohdan.ru>
2024-08-07 00:19:49 +0300

    Add birch gathering, hardwood plank crafting, fight yellow slime

--- cycle_carpenter.py
+++ cycle_carpenter.py
@@ -3,12 +3,19 @@ character_type ="carpenter"
 with open("functions.py") as functions:
     exec(functions.read())
 
-ash_limit = 52
-ash_plank_qty = 8
-ash_wood_qty = 4
+# 6 (ash_plank) + 4 (wooden_stick) + 2 (hardwood_plank)
+ash_limit = 44
+ash_plank_qty = 5
+ash_wood_qty = 2
 
-spruce_limit = 48
-spruce_plank_qty = 8
+# 6
+spruce_limit = 36
+spruce_plank_qty = 6
+
+# x4
+birch_limit = 24
+hardwood_plank_qty = 6
+
 
 while True:
 
@@ -26,6 +33,12 @@ while True:
     do_move(x, y)
     cycle_gathering(spruce_limit)
 
+    # birch tree (woodcutting 20)
+    print("=== gather birch tree ===")
+    x, y = 3, 5
+    do_move(x, y)
+    cycle_gathering(birch_limit)
+
     # ======= CRAFTING ======
 
     # craft ash plank, spruce_plank
@@ -34,6 +47,7 @@ while True:
     do_move(x, y)
     cycle_crafting("ash_plank", ash_plank_qty)
     cycle_crafting("spruce_plank", spruce_plank_qty)
+    cycle_crafting("hardwood_plank", hardwood_plank_qty)
 
     # ======= FIGHTING ======
 
@@ -41,8 +55,22 @@ while True:
     print ("=== fight chicken ===")
     x, y = 0, 1
     do_move(x, y)
-    #do_unequip("weapon")
+    do_unequip("weapon")
+    do_unequip("body_armor")
     #do_equip("wooden_staff", "weapon")
+    do_equip("sticky_sword", "weapon")
+    do_equip("copper_armor", "body_armor")
+    cycle_fight(10)
+
+    # yellow slime (2)
+    print ("=== fight yellow slime ===")
+    x, y = 1, -2
+    do_move(x, y)
+    do_unequip("weapon")
+    do_unequip("body_armor")
+    #do_equip("copper_dagger", "weapon")
+    do_equip("sticky_dagger", "weapon")
+    do_equip("feather_coat", "body_armor")
     cycle_fight(10)
 
     # ======= BANKING ======
@@ -55,9 +83,12 @@ while True:
     do_bank_deposit("ash_wood", ash_wood_qty)
     do_bank_deposit("ash_plank", ash_plank_qty)
     do_bank_deposit("spruce_plank", spruce_plank_qty)
+    do_bank_deposit("hardwood_plank", hardwood_plank_qty)
 
     do_bank_deposit("feather", 5)
     do_bank_deposit("egg", 5)
     do_bank_deposit("raw_chicken", 5)
     do_bank_deposit("golden_egg", 1)
+
+    do_bank_deposit("yellow_slimeball", 5)
 
