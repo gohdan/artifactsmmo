d8ce423dc49c2949969fcbabdae1f973cf0e1e43
gohdan <gohdan@gohdan.ru>
2024-08-07 00:28:27 +0300

    Add coal gathering, steel crafting, fight yellow slime

--- cycle_miner.py
+++ cycle_miner.py
@@ -3,11 +3,17 @@ character_type ="miner"
 with open("functions.py") as functions:
     exec(functions.read())
 
-copper_limit = 48
-copper_qty = 8
+# x6
+copper_limit = 30
+copper_qty = 5
 
-iron_limit = 48
-iron_qty = 8
+# x6 + 4 * steel_qty
+iron_limit = 30 + 20
+iron_qty = 5
+
+# x4
+coal_limit = 20
+steel_qty = 5
 
 while True:
 
@@ -25,6 +31,12 @@ while True:
     do_move(x, y)
     cycle_gathering(iron_limit)
 
+    # coal (mining 20)
+    print("=== gather coal ===")
+    x, y = 1, 6
+    do_move(x, y)
+    cycle_gathering(coal_limit)
+
     # ======= CRAFTING ======
 
     # craft copper, iron
@@ -33,6 +45,7 @@ while True:
     do_move(x, y)
     cycle_crafting("copper", copper_qty)
     cycle_crafting("iron", iron_qty)
+    cycle_crafting("steel", steel_qty)
 
     # ======= FIGHTING ======
 
@@ -40,8 +53,22 @@ while True:
     print ("=== fight chicken ===")
     x, y = 0, 1
     do_move(x, y)
-    #do_unequip("weapon")
+    do_unequip("weapon")
+    do_unequip("body_armor")
     #do_equip("wooden_staff", "weapon")
+    do_equip("sticky_sword", "weapon")
+    do_equip("copper_armor", "body_armor")
+    cycle_fight(10)
+
+    # yellow slime (2)
+    print ("=== fight yellow slime ===")
+    x, y = 1, -2
+    do_move(x, y)
+    do_unequip("weapon")
+    do_unequip("body_armor")
+    #do_equip("copper_dagger", "weapon")
+    do_equip("sticky_dagger", "weapon")
+    do_equip("feather_coat", "body_armor")
     cycle_fight(10)
 
     # ======= BANKING ======
@@ -54,9 +81,11 @@ while True:
     # just in case
     do_bank_deposit("copper_ore", 1)
     do_bank_deposit("iron_ore", 1)
+    do_bank_deposit("coal", 1)
 
     do_bank_deposit("copper", copper_qty)
     do_bank_deposit("iron", iron_qty)
+    do_bank_deposit("steel", steel_qty)
 
     do_bank_deposit("topaz", 1)
     do_bank_deposit("emerald", 1)
@@ -67,4 +96,6 @@ while True:
     do_bank_deposit("egg", 5)
     do_bank_deposit("raw_chicken", 5)
     do_bank_deposit("golden_egg", 1)
+
+    do_bank_deposit("yellow_slimeball", 5)
 
