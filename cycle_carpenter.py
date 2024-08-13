7b8d257c6461c5fc7e4af0b8de62f43651231d93
gohdan <gohdan@gohdan.ru>
2024-08-14 02:37:07 +0300

    Rebalance resources, add fight blue slime

--- cycle_carpenter.py
+++ cycle_carpenter.py
@@ -4,34 +4,22 @@ with open("functions.py") as functions:
     exec(functions.read())
 
 # 6 (ash_plank) + 4 (wooden_stick) + 2 (hardwood_plank)
-ash_limit = 44
-ash_plank_qty = 5
-ash_wood_qty = 2
+ash_limit = 42
+ash_plank_qty = 6
+ash_wood_qty = 6
 
 # 6
 spruce_limit = 36
 spruce_plank_qty = 6
 
 # x4
-birch_limit = 24
-hardwood_plank_qty = 6
+birch_limit = 28
+hardwood_plank_qty = 7
 
 
 while True:
 
     # ======= GATHERING ======
-
-    # ash tree (woodcutting 1)
-    print("=== gather ash tree ===")
-    x, y = -1, 0
-    do_move(x, y)
-    cycle_gathering(ash_limit)
-
-    # spruce tree (woodcutting 10)
-    print("=== gather spruce tree ===")
-    x, y = 2, 6
-    do_move(x, y)
-    cycle_gathering(spruce_limit)
 
     # birch tree (woodcutting 20)
     print("=== gather birch tree ===")
@@ -39,17 +27,40 @@ while True:
     do_move(x, y)
     cycle_gathering(birch_limit)
 
+    # spruce tree (woodcutting 10)
+    print("=== gather spruce tree ===")
+    x, y = 2, 6
+    do_move(x, y)
+    cycle_gathering(spruce_limit)
+
+    # ash tree (woodcutting 1)
+    print("=== gather ash tree ===")
+    x, y = -1, 0
+    do_move(x, y)
+    cycle_gathering(ash_limit)
+
     # ======= CRAFTING ======
 
-    # craft ash plank, spruce_plank
+    # craft ash plank, spruce_plank, hardwood plank
     print("move to workshop woodcutting")
     x, y = -2, -3
     do_move(x, y)
-    cycle_crafting("ash_plank", ash_plank_qty)
-    cycle_crafting("spruce_plank", spruce_plank_qty)
     cycle_crafting("hardwood_plank", hardwood_plank_qty)
+    cycle_crafting("spruce_plank", spruce_plank_qty)
+    cycle_crafting("ash_plank", ash_plank_qty)
 
     # ======= FIGHTING ======
+
+    # blue slime (6)
+    print ("=== fight blue slime ===")
+    x, y = 0, -2
+    do_move(x, y)
+    do_unequip("weapon")
+    do_unequip("body_armor")
+    #do_equip("wooden_staff", "weapon")
+    do_equip("sticky_sword", "weapon")
+    do_equip("copper_armor", "body_armor")
+    cycle_fight(10)
 
     # green slime (4)
     print ("=== fight green slime ===")
@@ -96,11 +107,12 @@ while True:
     do_bank_deposit("spruce_plank", spruce_plank_qty)
     do_bank_deposit("hardwood_plank", hardwood_plank_qty)
 
-    do_bank_deposit("feather", 5)
-    do_bank_deposit("egg", 5)
-    do_bank_deposit("raw_chicken", 5)
+    do_bank_deposit("feather", 4)
+    do_bank_deposit("egg", 4)
+    do_bank_deposit("raw_chicken", 4)
     do_bank_deposit("golden_egg", 1)
 
-    do_bank_deposit("yellow_slimeball", 5)
-    do_bank_deposit("green_slimeball", 5)
+    do_bank_deposit("yellow_slimeball", 4)
+    do_bank_deposit("green_slimeball", 4)
+    do_bank_deposit("blue_slimeball", 4)
 
