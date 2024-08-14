7b7ab36b9492c63fd4d72367d315154527c8ee2c
gohdan <gohdan@gohdan.ru>
2024-08-14 03:49:59 +0300

    Add fight flying serpent

--- cycle_main.py
+++ cycle_main.py
@@ -7,6 +7,16 @@ while True:
 
     # ======= FIGHTING ======
 
+    # flying serpent (10)
+    # pring ("=== fight flying serpent ===")
+    x, y = 5, 4
+    do_move(x, y)
+    do_unequip("weapon")
+    do_unequip("body_armor")
+    do_equip("greater_wooden_staff", "weapon")
+    do_equip("feather_coat", "body_armor")
+    cycle_fight(10)
+
     # mushmush (10)
     print ("=== fight mushmush ===")
     x, y = 5, 3
@@ -38,15 +48,15 @@ while True:
     cycle_fight(10)
 
     # blue slime (6)
-    print ("=== fight blue slime ===")
-    x, y = 0, -2
-    do_move(x, y)
-    do_unequip("weapon")
-    do_unequip("body_armor")
-    #do_equip("wooden_staff", "weapon")
-    do_equip("sticky_sword", "weapon")
-    do_equip("copper_armor", "body_armor")
-    cycle_fight(10)
+    #print ("=== fight blue slime ===")
+    #x, y = 0, -2
+    #do_move(x, y)
+    #do_unequip("weapon")
+    #do_unequip("body_armor")
+    ##do_equip("wooden_staff", "weapon")
+    #do_equip("sticky_sword", "weapon")
+    #do_equip("copper_armor", "body_armor")
+    #cycle_fight(10)
 
     # green slime (4)
     #print ("=== fight green slime ===")
@@ -96,12 +106,14 @@ while True:
 
     #do_bank_deposit("yellow_slimeball", 5)
     #do_bank_deposit("green_slimeball", 5)
-    do_bank_deposit("blue_slimeball", 5)
+    #do_bank_deposit("blue_slimeball", 5)
     do_bank_deposit("red_slimeball", 5)
     do_bank_deposit("raw_beef", 5)
     do_bank_deposit("milk_bucket", 5)
     do_bank_deposit("cowhide", 5)
     do_bank_deposit("mushroom", 5)
+    do_bank_deposit("flying_wing", 1)
+    do_bank_deposit("serpent_skin", 1)
 
     do_bank_withdraw("cooked_beef", 10)
     do_bank_withdraw("cooked_chicken", 10)
