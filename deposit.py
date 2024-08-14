1284bfe418a673704bacb1aeeae4228ef3cc4da5
gohdan <gohdan@gohdan.ru>
2024-08-14 03:50:38 +0300

    Update bank functions

--- deposit.py
+++ deposit.py
@@ -8,44 +8,74 @@ print ("=== deposit to bank ===")
 x, y = 4, 1
 do_move(x, y)
 
-#do_bank_deposit("ash_wood", 24)
+# woodcutting
+
+#do_bank_deposit("ash_wood", 62)
 #do_bank_deposit("spruce_wood", 4)
+#do_bank_deposit("birch_wood", 2)
+
+#do_bank_deposit("ash_plank", 23)
+#do_bank_deposit("spruce_plank", 2)
+#do_bank_deposit("hardwood_plank", 3)
+
+# mining
+
 #do_bank_deposit("copper_ore", 14)
-#do_bank_deposit("feather", 1)
-#do_bank_deposit("egg", 1)
-#do_bank_deposit("raw_chicken", 1)
-#do_bank_deposit("gudgeon", 2)
-#do_bank_deposit("yellow_slimeball", 2)
-#do_bank_deposit("green_slimeball", 1)
-#do_bank_deposit("blue_slimeball", 4)
+#do_bank_deposit("copper", 2)
+#do_bank_deposit("iron_ore", 27)
+#do_bank_deposit("iron", 20)
+#do_bank_deposit("steel", 3)
 
-#do_bank_deposit("copper", 32)
+# fishing
 
-#do_bank_deposit("ash_plank", 1)
-#do_bank_deposit("spruce_plank", 1)
+#do_bank_deposit("gudgeon", 30)
+#do_bank_deposit("shrimp", 26)
+#do_bank_deposit("trout", 27)
+
+
+
+#do_bank_deposit("feather", 4)
+#do_bank_deposit("egg", 2)
+#do_bank_deposit("raw_chicken", 2)
+#do_bank_deposit("raw_beef", 3)
+#do_bank_deposit("milk_bucket", 5)
+#do_bank_deposit("cowhide", 4)
+
+#do_bank_deposit("yellow_slimeball", 4)
+#do_bank_deposit("green_slimeball", 4)
+#do_bank_deposit("blue_slimeball", 1)
+#do_bank_deposit("red_slimeball", 1)
+
+# weapon
 
 #do_bank_deposit("wooden_stick", 1)
-#do_bank_deposit("wooden_shield", 1)
 #do_bank_deposit("wooden_staff", 1)
 #do_bank_deposit("copper_dagger", 1)
-#do_bank_deposit("copper_helmet", 1)
-#do_bank_deposit("copper_boots", 1)
-
-#do_bank_deposit("feather_coat", 1)
-#do_bank_deposit("copper_armor", 1)
-#do_bank_deposit("copper_legs_armor", 1)
 #do_bank_deposit("sticky_dagger", 1)
-#do_bank_deposit("sticky_sword", 1)
+#do_bank_deposit("sticky_sword", 20)
 #do_bank_deposit("water_bow", 1)
 #do_bank_deposit("fire_staff", 1)
-
 #do_bank_deposit("iron_dagger", 1)
+#do_bank_deposit("greater_wooden_staff", 1)
+
+# gear
+
+#do_bank_deposit("wooden_shield", 1)
+#do_bank_deposit("copper_helmet", 1)
+#do_bank_deposit("copper_boots", 1)
+#do_bank_deposit("feather_coat", 76)
+#do_bank_deposit("copper_armor", 20)
+#do_bank_deposit("copper_legs_armor", 1)
+
+# jewelry
 
 #do_bank_deposit("copper_ring", 7)
 #do_bank_deposit("life_amulet", 1)
 
+# cooking
+
 #do_bank_deposit("cooked_gudgeon", 1)
 #do_bank_deposit("cooked_chicken", 1)
 #do_bank_deposit("cooked_beef", 1)
-
+#do_bank_deposit("cooked_shrimp", 1)
 
