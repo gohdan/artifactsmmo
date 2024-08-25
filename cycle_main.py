6036453c3988653ee4a9b694929d5626ccbfdf67
gohdan <gohdan@gohdan.ru>
2024-08-25 22:00:50 +0300

    Update main cycle to level 1

--- cycle_main.py
+++ cycle_main.py
@@ -7,47 +7,47 @@ while True:
 
     # ======= FIGHTING ======
 
-    # flying serpent (10)
-    print ("=== fight flying serpent ===")
-    x, y = 5, 4
-    do_move(x, y)
-    do_unequip("weapon")
-    do_unequip("body_armor")
-    do_equip("greater_wooden_staff", "weapon")
-    do_equip("feather_coat", "body_armor")
-    cycle_fight(10)
+    ## flying serpent (10)
+    #print ("=== fight flying serpent ===")
+    #x, y = 5, 4
+    #do_move(x, y)
+    #do_unequip("weapon")
+    #do_unequip("body_armor")
+    #do_equip("greater_wooden_staff", "weapon")
+    #do_equip("feather_coat", "body_armor")
+    #cycle_fight(10)
 
-    # mushmush (10)
-    print ("=== fight mushmush ===")
-    x, y = 5, 3
-    do_move(x, y)
-    do_unequip("weapon")
-    do_unequip("body_armor")
-    do_equip("iron_dagger", "weapon")
-    do_equip("feather_coat", "body_armor")
-    cycle_fight(10)
+    ## mushmush (10)
+    #print ("=== fight mushmush ===")
+    #x, y = 5, 3
+    #do_move(x, y)
+    #do_unequip("weapon")
+    #do_unequip("body_armor")
+    #do_equip("iron_dagger", "weapon")
+    #do_equip("feather_coat", "body_armor")
+    #cycle_fight(10)
 
     # cow (8)
-    print ("=== fight cow ===")
-    x, y = 0, 2
-    do_move(x, y)
-    do_unequip("weapon")
-    do_unequip("body_armor")
-    #do_equip("sticky_sword", "weapon")
-    do_equip("iron_sword", "weapon")
-    do_equip("copper_armor", "body_armor")
-    cycle_fight(10)
+    #print ("=== fight cow ===")
+    #x, y = 0, 2
+    #do_move(x, y)
+    #do_unequip("weapon")
+    #do_unequip("body_armor")
+    ##do_equip("sticky_sword", "weapon")
+    #do_equip("iron_sword", "weapon")
+    #do_equip("copper_armor", "body_armor")
+    #cycle_fight(10)
     
-    # red slime (7)
-    print ("=== fight red slime ===")
-    x, y = 2, -2
-    do_move(x, y)
-    do_unequip("weapon")
-    do_unequip("body_armor")
-    #do_equip("sticky_sword", "weapon")
-    do_equip("iron_sword", "weapon")
-    do_equip("copper_armor", "body_armor")
-    cycle_fight(10)
+    ## red slime (7)
+    #print ("=== fight red slime ===")
+    #x, y = 2, -2
+    #do_move(x, y)
+    #do_unequip("weapon")
+    #do_unequip("body_armor")
+    ##do_equip("sticky_sword", "weapon")
+    #do_equip("iron_sword", "weapon")
+    #do_equip("copper_armor", "body_armor")
+    #cycle_fight(10)
 
     # blue slime (6)
     #print ("=== fight blue slime ===")
@@ -84,15 +84,15 @@ while True:
     #cycle_fight(10)
 
     # chicken (1)
-    #print ("=== fight chicken ===")
-    #x, y = 0, 1
-    #do_move(x, y)
+    print ("=== fight chicken ===")
+    x, y = 0, 1
+    do_move(x, y)
     #do_unequip("weapon")
     #do_unequip("body_armor")
     ##do_equip("wooden_staff", "weapon")
     #do_equip("sticky_sword", "weapon")
     #do_equip("copper_armor", "body_armor")
-    #cycle_fight(10)
+    cycle_fight(10)
 
     # ======= BANKING ======
 
@@ -101,23 +101,23 @@ while True:
     x, y = 4, 1
     do_move(x, y)
 
-    #do_bank_deposit("feather", 5)
-    #do_bank_deposit("egg", 5)
-    #do_bank_deposit("raw_chicken", 5)
-    #do_bank_deposit("golden_egg", 1)
+    do_bank_deposit("feather", 5)
+    do_bank_deposit("egg", 5)
+    do_bank_deposit("raw_chicken", 5)
+    do_bank_deposit("golden_egg", 1)
 
     #do_bank_deposit("yellow_slimeball", 5)
     #do_bank_deposit("green_slimeball", 5)
     #do_bank_deposit("blue_slimeball", 5)
-    do_bank_deposit("red_slimeball", 5)
-    do_bank_deposit("raw_beef", 5)
-    do_bank_deposit("milk_bucket", 5)
-    do_bank_deposit("cowhide", 5)
-    do_bank_deposit("mushroom", 5)
-    do_bank_deposit("flying_wing", 1)
-    do_bank_deposit("serpent_skin", 1)
+    #do_bank_deposit("red_slimeball", 5)
+    #do_bank_deposit("raw_beef", 5)
+    #do_bank_deposit("milk_bucket", 5)
+    #do_bank_deposit("cowhide", 5)
+    #do_bank_deposit("mushroom", 5)
+    #do_bank_deposit("flying_wing", 1)
+    #do_bank_deposit("serpent_skin", 1)
 
-    do_bank_withdraw("cooked_beef", 10)
-    do_bank_withdraw("cooked_chicken", 10)
-    do_bank_withdraw("cooked_gudgeon", 10)
+    #do_bank_withdraw("cooked_beef", 10)
+    #do_bank_withdraw("cooked_chicken", 10)
+    #do_bank_withdraw("cooked_gudgeon", 10)
 
