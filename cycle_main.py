fd2b2363a0d1b04446be52a5ae4c5f09e533f4b8
gohdan <gohdan@gohdan.ru>
2025-07-12 02:15:51 +0300

    Move cow to higher level

--- cycle_main.py
+++ cycle_main.py
@@ -201,17 +201,17 @@ while True:
             print ("=== fight sheep ===")
             go_fight("sheep", 10)
 
-        case level if 9 <= level < 10:
+        case level if 9 <= level < 11:
             # red slime (7)
             print ("=== fight red slime ===")
             go_fight("red_slime", 10)
 
-        case level if 10 <= level < 11:
+        case level if 11 <= level < 12:
             # cow (8)
             print ("=== fight cow ===")
             go_fight("cow", 10)
 
-        case level if 11 <= level:
+        case level if 12 <= level:
             # mushmush (10)
             print ("=== fight mushmush ===")
             go_fight("mushmush", 10)
