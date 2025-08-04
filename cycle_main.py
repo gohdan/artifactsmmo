1603d949bf9b0c4629428b6b18987cf45f8e734e
gohdan <gohdan@gohdan.ru>
2025-08-04 03:44:34 +0300

    Add fight skeleton

--- cycle_main.py
+++ cycle_main.py
@@ -227,7 +227,7 @@ while True:
             print ("=== fight flying_snake ===")
             go_fight("flying_snake", 10)
 
-        case level if 23 <= level:
+        case level if 23 <= level < 26:
             import random
             randval = random.randint(1, 2)
             if 1 == randval:
@@ -238,6 +238,11 @@ while True:
                 # highwayman (15)
                 print ("=== fight highwayman ===")
                 go_fight("highwayman", 10)
+
+        case level if 26 <= level:
+                # skeleton (18)
+                print ("=== fight skeleton ===")
+                go_fight("skeleton", 10)
 
         case _:
             # default values
--- functions.py
+++ functions.py
@@ -1305,6 +1305,75 @@ def do_equip_to_monster(monster):
             elif "life_amulet" in inventory_items:
                 do_equip("life_amulet", "amulet")
 
+        case monster if "skeleton" == monster:
+            # -10% Res Water, -10% Res Air, 30% Res Fire, 26 Fire attack, 18 Earth attack
+
+            if "greater_wooden_staff" in inventory_items:
+                do_equip("greater_wooden_staff", "weapon")
+            elif "iron_dagger" in inventory_items:
+                do_equip("iron_dagger", "weapon")
+            elif "water_bow" in inventory_items:
+                do_equip("water_bow", "weapon")
+            elif "sticky_dagger" in inventory_items:
+                do_equip("sticky_dagger", "weapon")
+            elif "copper_dagger" in inventory_items:
+                do_equip("copper_dagger", "weapon")
+
+            if "slime_shield" in inventory_items:
+                do_equip("slime_shield", "shield")
+            elif "wooden_shield" in inventory_items:
+                do_equip("wooden_shield", "shield")
+
+            if "leather_armor" in inventory_items:
+                do_equip("leather_armor", "body_armor")
+            elif "iron_armor" in inventory_items:
+                do_equip("iron_armor", "body_armor")
+            elif "feather_coat" in inventory_items:
+                do_equip("feather_coat", "body_armor")
+            elif "copper_armor" in inventory_items:
+                do_equip("copper_armor", "body_armor")
+
+            if "leather_hat" in inventory_items:
+                do_equip("leather_hat", "helmet")
+            elif "iron_helm" in inventory_items:
+                do_equip("iron_helm", "helmet")
+            elif "copper_helmet" in inventory_items:
+                do_equip("copper_helmet", "helmet")
+
+            if "leather_legs_armor" in inventory_items:
+                do_equip("leather_legs_armor", "leg_armor")
+            elif "iron_legs_armor" in inventory_items:
+                do_equip("iron_legs_armor", "leg_armor")
+            elif "copper_legs_armor" in inventory_items:
+                do_equip("copper_legs_armor", "leg_armor")
+
+            if "iron_boots" in inventory_items:
+                do_equip("iron_boots", "boots")
+            elif "leather_boots" in inventory_items:
+                do_equip("leather_boots", "boots")
+            elif "copper_boots" in inventory_items:
+                do_equip("copper_boots", "boots")
+
+            if "iron_ring" in inventory_items:
+                do_equip("iron_ring", "ring1")
+                do_equip("iron_ring", "ring2")
+            elif "copper_ring" in inventory_items:
+                do_equip("copper_ring", "ring1")
+                do_equip("copper_ring", "ring2")
+
+            if "small_health_potion" in inventory_items:
+                do_equip("small_health_potion", "utility1")
+            if "air_boost_potion" in inventory_items:
+                do_equip("air_boost_potion", "utility2")
+
+            if "air_and_water_amulet" in inventory_items:
+                do_equip("air_and_water_amulet", "amulet")
+            elif "fire_and_earth_amulet" in inventory_items:
+                do_equip("fire_and_earth_amulet", "amulet")
+            elif "life_amulet" in inventory_items:
+                do_equip("life_amulet", "amulet")
+
+
 def get_inventory_items():
 
     print("*** get_inventory_items")
@@ -1543,6 +1612,8 @@ def go_to_monster(monster):
             x, y = -2, 1
         case monster if "highwayman" == monster:
             x, y = 2, 8
+        case monster if "skeleton" == monster:
+            x, y = 8, 6
 
     do_move(x,y)
 
